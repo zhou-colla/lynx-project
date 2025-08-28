dc30434ec578b642c54f0df113e9f645ff8a0ef4
ywllowsensor <isyazani@gmail.com>
2025-08-28 10:37:49 +0800

    feat: Update to include routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,18 @@
-import '@lynx-js/react/debug'
-import { root } from '@lynx-js/react'
+import { root } from '@lynx-js/react';
+import { MemoryRouter, Routes, Route } from 'react-router';
 
-import { App } from './App.jsx'
+import { App } from './App.js';
+import { Memory } from './components/memory/memory.js'
 
-root.render(<App />)
+root.render(
+  <MemoryRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/memory" element={<Memory />} />
+    </Routes>
+  </MemoryRouter>,
+);
 
 if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
+  import.meta.webpackHot.accept();
 }
